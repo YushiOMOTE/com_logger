9226510a7cbb365709455edfdc240b5335b3c96d
YushiOMOTE <yushiomote@gmail.com>
2022-08-31 07:54:11 +0900

    Remove external_doc and use include_str

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,6 @@
-//!
-//! `com_logger` is a logger through COM port.
-//!
-//! This library is `no_std`, and doesn't rely on `alloc`.
-//!
-//! ```rust,no_run
-//! use log::*;
-//!
-//! fn main() {
-//!    com_logger::init();
-//!
-//!    info!("Hello");
-//! }
-//! ```
-//!
-//! The serial port base address and logging level filter can be configured.
-//!
-//! ```rust,no_run
-//! use log::*;
-//!
-//! fn main() {
-//!    com_logger::builder()
-//!        .base(0x2f8)                  // Use COM2 port
-//!        .filter(LevelFilter::Debug)   // Print debug log
-//!        .setup();
-//!
-//!    debug!("Hello");
-//! }
-//! ```
-
 #![no_std]
-#![cfg_attr(feature = "readme", feature(external_doc))]
 #![warn(missing_docs)]
-
-#[cfg_attr(feature = "readme", doc(include = "../README.md"))]
-type _Readme = ();
+#![doc = include_str!("../README.md")]
 
 /// The module for the logger.
 mod logger;
