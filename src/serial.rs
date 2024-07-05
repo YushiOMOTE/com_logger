6eb84804e56e2818c7bec271fea826f9f1e4bc23
Wcscpy <107370797+Azvanzed@users.noreply.github.com>
2024-07-05 23:33:41 +0100

    Update serial.rs

--- src/serial.rs
+++ src/serial.rs
@@ -1,5 +1,4 @@
 use core::fmt;
-use spin::Mutex;
 use uart_16550::SerialPort;
 
 /// Serial port driver which implements [`core::fmt::Write`][].
@@ -19,37 +18,29 @@ use uart_16550::SerialPort;
 ///    core::fmt::write(&mut s, format_args!("Hello {}", 0xdead));
 /// }
 /// ```
-pub struct Serial {
-    port: Mutex<SerialPort>,
-}
+pub struct Serial(SerialPort);
 
 impl Serial {
     /// Create the driver instance for the specified base address.
     pub fn new(base: u16) -> Self {
-        let port = unsafe { SerialPort::new(base) };
-
-        Self {
-            port: Mutex::new(port),
-        }
+        Self(unsafe { SerialPort::new(base) })
     }
 
     /// Initialize the serial port.
     pub fn init(&mut self) {
-        self.port.lock().init();
+        self.0.init();
     }
 
     /// Write a single byte to the serial port.
     pub fn write(&mut self, d: u8) {
-        self.port.lock().send(d);
+        self.0.send(d);
     }
 }
 
 impl fmt::Write for Serial {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        let mut port = self.port.lock();
-
         for b in s.bytes() {
-            port.send(b);
+            self.0.send(b);
         }
         Ok(())
     }
